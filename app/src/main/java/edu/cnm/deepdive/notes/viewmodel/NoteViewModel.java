031121e6547d6d0cc78dcda880fe8fe620f9f1c1
nick-bennett <nbennett10@cnm.edu>
2025-02-12 11:59:04 -0700

    NoteViewModel mostly complete.

--- app/src/main/java/edu/cnm/deepdive/notes/viewmodel/NoteViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/notes/viewmodel/NoteViewModel.java
@@ -1,26 +1,34 @@
 package edu.cnm.deepdive.notes.viewmodel;
 
 import android.util.Log;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 import dagger.hilt.android.lifecycle.HiltViewModel;
 import edu.cnm.deepdive.notes.model.entity.Note;
 import edu.cnm.deepdive.notes.service.NoteRepository;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import java.util.List;
 import javax.inject.Inject;
+import org.jetbrains.annotations.NotNull;
 
 @HiltViewModel
-public class NoteViewModel extends ViewModel {
+public class NoteViewModel extends ViewModel implements DefaultLifecycleObserver {
 
   private final NoteRepository noteRepository;
-  private final MutableLiveData<Note> note;
+  private final MutableLiveData<Long> noteId;
+  private final LiveData<Note> note;
   private final MutableLiveData<Throwable> throwable;
   private final CompositeDisposable pending;
 
   @Inject
   NoteViewModel(NoteRepository noteRepository) {
     this.noteRepository = noteRepository;
-    note = new MutableLiveData<>();
+    noteId = new MutableLiveData<>();
+    note = Transformations.switchMap(noteId, noteRepository::get);
     throwable = new MutableLiveData<>();
     pending = new CompositeDisposable();
   }
@@ -29,15 +37,50 @@ public class NoteViewModel extends ViewModel {
     throwable.setValue(null); // Will be invoked from controller on UI thread.
     noteRepository
         .save(note)
+        .ignoreElement()
         .subscribe(
-            this.note::postValue,
+            () -> {},
             this::postThrowable,
             pending
         );
   }
 
   public void fetch(long noteId) {
+    throwable.setValue(null);
+    this.noteId.setValue(noteId);
+  }
 
+  public void delete(Note note) {
+    throwable.setValue(null);
+    noteRepository
+        .remove(note)
+        .subscribe(
+            () ->{},
+            this::postThrowable,
+            pending
+        );
+  }
+
+  public MutableLiveData<Long> getNoteId() {
+    return noteId;
+  }
+
+  public LiveData<Note> getNote() {
+    return note;
+  }
+
+  public LiveData<List<Note>> getNotes() {
+    return noteRepository.getAll();
+  }
+
+  public MutableLiveData<Throwable> getThrowable() {
+    return throwable;
+  }
+
+  @Override
+  public void onStop(@NotNull LifecycleOwner owner) {
+    pending.clear();
+    DefaultLifecycleObserver.super.onStop(owner);
   }
 
   private void postThrowable(Throwable throwable) {
