179b60acc78da27fa718751d4d0b3d7c10e90fb8
nick-bennett <nbennett10@cnm.edu>
2025-02-12 10:53:57 -0700

    Working on NoteViewModel.

--- app/src/main/java/edu/cnm/deepdive/notes/viewmodel/NoteViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/notes/viewmodel/NoteViewModel.java
@@ -1,18 +1,48 @@
 package edu.cnm.deepdive.notes.viewmodel;
 
+import android.util.Log;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import edu.cnm.deepdive.notes.model.entity.Note;
 import edu.cnm.deepdive.notes.service.NoteRepository;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import javax.inject.Inject;
 
 @HiltViewModel
 public class NoteViewModel extends ViewModel {
 
   private final NoteRepository noteRepository;
+  private final MutableLiveData<Note> note;
+  private final MutableLiveData<Throwable> throwable;
+  private final CompositeDisposable pending;
 
   @Inject
   NoteViewModel(NoteRepository noteRepository) {
     this.noteRepository = noteRepository;
+    note = new MutableLiveData<>();
+    throwable = new MutableLiveData<>();
+    pending = new CompositeDisposable();
+  }
+
+  public void save(Note note) {
+    throwable.setValue(null); // Will be invoked from controller on UI thread.
+    noteRepository
+        .save(note)
+        .subscribe(
+            this.note::postValue,
+            this::postThrowable,
+            pending
+        );
+  }
+
+  public void fetch(long noteId) {
+
+  }
+
+  private void postThrowable(Throwable throwable) {
+    Log.e(NoteViewModel.class.getSimpleName(), throwable.getMessage(), throwable);
+    this.throwable.postValue(throwable);
   }
 
 }
