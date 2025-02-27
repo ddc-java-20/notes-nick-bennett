85099af0f095eb4cdc72ef4cab805fb3200c5c63
nick-bennett <nbennett10@cnm.edu>
2025-02-27 16:52:45 -0700

    Full sign-in flow working.

--- app/src/main/java/edu/cnm/deepdive/notes/controller/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/notes/controller/HomeFragment.java
@@ -4,32 +4,38 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.PopupMenu;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle.State;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import dagger.hilt.android.AndroidEntryPoint;
 import edu.cnm.deepdive.notes.R;
 import edu.cnm.deepdive.notes.adapter.NotesAdapter;
 import edu.cnm.deepdive.notes.databinding.FragmentHomeBinding;
 import edu.cnm.deepdive.notes.model.entity.Note;
+import edu.cnm.deepdive.notes.viewmodel.LoginViewModel;
 import edu.cnm.deepdive.notes.viewmodel.NoteViewModel;
 import java.util.List;
 
+/** @noinspection deprecation*/
 @AndroidEntryPoint
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements MenuProvider {
 
   private static final String TAG = HomeFragment.class.getSimpleName();
 
   private FragmentHomeBinding binding;
-  private NoteViewModel viewModel;
+  private NoteViewModel noteViewModel;
+  private LoginViewModel loginViewModel;
 
   @Override
   public View onCreateView(
@@ -44,10 +50,16 @@ public class HomeFragment extends Fragment {
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
     LifecycleOwner lifecycleOwner = getViewLifecycleOwner();
-    viewModel = new ViewModelProvider(requireActivity()).get(NoteViewModel.class);
-    viewModel
+    ViewModelProvider provider = new ViewModelProvider(requireActivity());
+    noteViewModel = provider.get(NoteViewModel.class);
+    noteViewModel
         .getNotes()
         .observe(lifecycleOwner, this::handleNotes);
+    loginViewModel = provider.get(LoginViewModel.class);
+    loginViewModel
+        .getAccount()
+        .observe(lifecycleOwner, this::handleAccount);
+    requireActivity().addMenuProvider(this, lifecycleOwner, State.RESUMED);
   }
 
   @Override
@@ -74,7 +86,7 @@ public class HomeFragment extends Fragment {
           .findItem(R.id.delete_note)
           .setOnMenuItemClickListener((item) -> {
             Log.d(TAG, String.format("onMenuItemClick: item=%s", item.getTitle()));
-            viewModel.delete(note);
+            noteViewModel.delete(note);
 //            adapter.notifyItemRemoved(position);
             return true;
           });
@@ -82,5 +94,28 @@ public class HomeFragment extends Fragment {
       return true;
     });
     binding.notes.setAdapter(adapter);
+  }
+
+  private void handleAccount(GoogleSignInAccount account) {
+    if (account == null) {
+      Navigation.findNavController(binding.getRoot())
+          .navigate(HomeFragmentDirections.navigateToPreLoginFragment());
+    }
+  }
+
+  @Override
+  public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+    menuInflater.inflate(R.menu.note_actions, menu);
+  }
+
+  @Override
+  public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+    boolean handled = true;
+    if (menuItem.getItemId() == R.id.sign_out) {
+      loginViewModel.signOut();
+    } else {
+      handled = false;
+    }
+    return handled;
   }
 }
