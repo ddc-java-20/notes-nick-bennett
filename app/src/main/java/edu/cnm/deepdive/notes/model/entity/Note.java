c56abc33d9e7b4ab02e94d5fbdbbc1e2e01bed68
nick-bennett <nbennett10@cnm.edu>
2025-03-11 10:07:02 -0600

    Adding user entity.

--- app/src/main/java/edu/cnm/deepdive/notes/model/entity/Note.java
+++ app/src/main/java/edu/cnm/deepdive/notes/model/entity/Note.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 import com.google.gson.annotations.Expose;
@@ -14,6 +15,10 @@ import java.time.Instant;
     tableName = "note",
     indices = {
         @Index(value = {"title"}, unique = true)
+    },
+    foreignKeys = {
+        @ForeignKey(entity = User.class, parentColumns = "user_id", childColumns = "user_id",
+            onDelete = ForeignKey.CASCADE)
     }
 )
 public class Note {
@@ -21,6 +26,9 @@ public class Note {
   @PrimaryKey(autoGenerate = true)
   @ColumnInfo(name = "note_id")
   private long id;
+
+  @ColumnInfo(name = "user_id", index = true)
+  private long userId;
 
   @ColumnInfo(collate = ColumnInfo.NOCASE)
   @Expose
@@ -48,6 +56,14 @@ public class Note {
 
   public void setId(long id) {
     this.id = id;
+  }
+
+  public long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(long userId) {
+    this.userId = userId;
   }
 
   @NonNull
